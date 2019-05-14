07cfb5153cf4d3bd4cb70d6c17e1e33841671c87
ilyaKhim <ilya.himchenko@yandex.ru>
2019-05-14 22:37:32 +0600

    init 2 arr, need to do 2 threads

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,34 +1,40 @@
 import java.util.Arrays;
 
 public class MyThread  {
-    static final int SIZE = 10;
+    static final int SIZE = 1_0;
     static final int HALF = SIZE/2;
     float[] arr = new float[SIZE];
 
-    public float[] makeDefaultArr(){
-
-        boolean fullOfOne = false;
-        for(int i=0; i<SIZE; i++){
-            arr[i] = 1;
-        }
+    private float[] makeDefaultArr(){
+        Arrays.fill(arr, 1);
         long begin = System.currentTimeMillis();
-
-        fullOfOne = true;
-        if (fullOfOne){
-            for(int i = 0; i<SIZE; i++){
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
+        for(int i = 0; i<SIZE; i++){
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        long end = System.currentTimeMillis();
-        System.out.println( end - begin);
-
+        System.out.println( System.currentTimeMillis() - begin);
         return arr;
+    }
+
+    private void  makeCoolArr() {
+        Arrays.fill(arr,1);
+        long begin = System.currentTimeMillis();
+        System.out.println(begin);
+        float[] coolArr1 = new float[HALF];
+        float[] coolArr2 = new float[HALF];
+        System.arraycopy(arr,0,coolArr1,0,HALF);
+        System.arraycopy(arr,HALF,coolArr2,0,HALF);
+        System.out.println(Arrays.toString(coolArr1) + "1");
+        System.out.println(Arrays.toString(coolArr2) + "2");
     }
 
     public static void main(String[] args) {
         MyThread thread1 = new MyThread();
         float[] defaultArr = thread1.makeDefaultArr();
-        System.out.println(Arrays.toString(defaultArr));
+//        System.out.println(Arrays.toString(defaultArr));
+        MyThread thread2 = new MyThread();
+        thread2.makeCoolArr();
 
     }
+
+
 }
