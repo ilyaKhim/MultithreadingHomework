787bee4d9555959fa08bf877d12a7c7f921bc81d
ilyaKhim <ilya.himchenko@yandex.ru>
2019-05-14 23:30:17 +0600

    done

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,30 +1,64 @@
 import java.util.Arrays;
 
-public class MyThread  {
-    static final int SIZE = 1_0;
-    static final int HALF = SIZE/2;
+public class MyThread {
+    static final int SIZE = 1_000_000;
+    static final int HALF = SIZE / 2;
     float[] arr = new float[SIZE];
 
-    private float[] makeDefaultArr(){
+    private float[] makeDefaultArr() {
         Arrays.fill(arr, 1);
         long begin = System.currentTimeMillis();
-        for(int i = 0; i<SIZE; i++){
+        for (int i = 0; i < SIZE; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println( System.currentTimeMillis() - begin);
+        System.out.println(System.currentTimeMillis() - begin);
         return arr;
     }
 
-    private void  makeCoolArr() {
-        Arrays.fill(arr,1);
+    private synchronized float[] makeCoolArr() {
+        Arrays.fill(arr, 1);
         long begin = System.currentTimeMillis();
-        System.out.println(begin);
         float[] coolArr1 = new float[HALF];
         float[] coolArr2 = new float[HALF];
-        System.arraycopy(arr,0,coolArr1,0,HALF);
-        System.arraycopy(arr,HALF,coolArr2,0,HALF);
-        System.out.println(Arrays.toString(coolArr1) + "1");
-        System.out.println(Arrays.toString(coolArr2) + "2");
+        System.arraycopy(arr, 0, coolArr2, 0, HALF);
+        System.arraycopy(arr, HALF, coolArr2, 0, HALF);
+
+        Thread thread = new MathThread(coolArr1);
+        Thread thread2 = new MathThread(coolArr2);
+
+        try {
+            thread.start();
+
+            thread2.start();
+            wait(150);
+
+            //thread.join();
+            //thread2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(coolArr1, 0, arr, 0, HALF);
+        System.arraycopy(coolArr2, 0, arr, HALF, HALF);
+        long end = System.currentTimeMillis();
+        System.out.println(end - begin);
+        return arr;
+    }
+
+    public class MathThread extends Thread {
+        private float[] data;
+
+        public MathThread(float[] data) {
+            this.data = data;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < HALF; i++) {
+                data[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+//                System.out.println(Thread.currentThread().getId() + " running");
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -32,8 +66,8 @@ public class MyThread  {
         float[] defaultArr = thread1.makeDefaultArr();
 //        System.out.println(Arrays.toString(defaultArr));
         MyThread thread2 = new MyThread();
-        thread2.makeCoolArr();
-
+        float[] coolArr = thread2.makeCoolArr();
+        System.out.println(Arrays.toString(coolArr));
     }
 
 
